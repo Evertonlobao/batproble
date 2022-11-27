4e1c36bf1bab22343554561c04c9638db8f93088
Evertonlobao <97298247+Evertonlobao@users.noreply.github.com>
2022-11-26 21:22:18 -0300

    language support

---
+++ app/eng-us/normal_time.bat
@@ -0,0 +1,7 @@
+@echo OFF
+echo well thanks for this 
+echo EPIC GOOD ENDING
+pause
+start C:\Users\Vanderley\Desktop\batproble\content\images\Thanks_For_Playing.png
+start C:\Users\Vanderley\Desktop\batproble\content\text\thanks_for_playing.txt
+exit
---
+++ app/eng-us/pc_access.bat
@@ -0,0 +1,36 @@
+@echo OFF
+color b
+echo how dare you bro?
+echo i just wanted to be friends
+echo why did you do this
+echo if you dont know im trying to have access in your pc right now
+echo just wait a second
+pause
+color a
+echo Pc_Acess.dll is loading...
+pause
+echo Pc_Acess.dll loaded
+pause
+color c
+echo done now i have access in your full pc
+echo i can see yours softwares,opened apps,files,and browser access
+pause
+start https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/-127wiki.jpg/1200px--127wiki.jpg
+pause
+color d
+echo HAHAHAHAHA I HAVE ACCESS IN EVERYTHING
+start https://img.ifunny.co/images/61150b49ae1c00376fc5d009630e2408f247744ac1a0ff4df2b8a26825b9c27f_1.jpg
+color b 
+echo YOU CANT ESCAPE FROM ME
+start https://fantoche.ch/sites/default/files/ta05_hi_stranger_31518_a_0.jpg
+start https://img.ifunny.co/images/fb7265092e2468a885761d0f0a0f5b737d2382d1fd47c97e680836eca5ff09cd_3.jpg
+start https://i.kym-cdn.com/photos/images/original/002/217/430/a59.jpg
+pause 
+echo no...
+echo this cant be i just pressed the wrong buttom
+echo this task will be closed but i will have my revange i will have it
+start C:\Users\Vanderley\Desktop\batproble\content\images\goodbye.png
+pause
+start C:\Users\Vanderley\Desktop\batproble\content\images\Thanks_For_Playing.png
+start C:\Users\Vanderley\Desktop\batproble\content\text\thanks_for_playing.txt
+exit
