d1b36d829e7ca45396b8b349af14544ca28573c7
Evertonlobao <97298247+Evertonlobao@users.noreply.github.com>
2022-11-26 21:22:57 -0300

    language support

---
+++ app/pt-br/normal_time.bat
@@ -0,0 +1,7 @@
+@echo OFF
+echo bom obrigado por isso
+echo EPICO FINAL BOM
+pause
+start C:\Users\Vanderley\Desktop\batproble\content\images\Thanks_For_Playing.png
+start C:\Users\Vanderley\Desktop\batproble\content\text\thanks_for_playing.txt
+exit
---
+++ app/pt-br/pc_access.bat
@@ -0,0 +1,35 @@
+@echo OFF
+color b
+echo como voce se atreve cara
+echo eu so queria ser amigo
+echo porque voce fez isso
+echo se você não sabe estou tentando ter acesso no seu pc agora
+echo so espere um seguse
+color a
+echo Pc_Acess.dll is loading...
+pause
+echo Pc_Acess.dll loaded
+pause
+color c
+echo feito agora tenho acesso no seu pc completo
+echo posso ver seus softwares, aplicativos abertos, arquivos e acesso ao navegador
+pause
+start https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/-127wiki.jpg/1200px--127wiki.jpg
+pause
+color d
+echo HAHAHAHAHA TENHO ACESSO EM TUDO
+start https://img.ifunny.co/images/61150b49ae1c00376fc5d009630e2408f247744ac1a0ff4df2b8a26825b9c27f_1.jpg
+color b 
+echo VOCE NAO PODE ESCAPAR DE MIM
+start https://fantoche.ch/sites/default/files/ta05_hi_stranger_31518_a_0.jpg
+start https://img.ifunny.co/images/fb7265092e2468a885761d0f0a0f5b737d2382d1fd47c97e680836eca5ff09cd_3.jpg
+start https://i.kym-cdn.com/photos/images/original/002/217/430/a59.jpg
+pause 
+echo nao...
+echo isso não pode ser eu só apertei o botão errado
+echo esta tarefa será encerrada, mas terei minha vingança, terei isso
+start C:\Users\Vanderley\Desktop\batproble\content\images\goodbye.png
+pause
+start C:\Users\Vanderley\Desktop\batproble\content\images\Thanks_For_Playing.png
+start C:\Users\Vanderley\Desktop\batproble\content\text\obrigado_por_jogar.txt
+exit
